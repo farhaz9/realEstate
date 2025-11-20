348f280b9fb262d85e7b99626bb163114494b699
Oracus Wisdom <thegreatfarhaz07@gmail.com>
2025-11-20 13:41:00 +0000

    Try fixing this error: `Build Error: Export Lottie doesn't exist in targ

--- src/components/shared/lottie-player.tsx
+++ src/components/shared/lottie-player.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { Lottie, LottieComponentProps } from "lottie-react";
+import Lottie, { LottieComponentProps } from "lottie-react";
 
 export default function LottiePlayer(props: LottieComponentProps) {
   const animationData =
-    typeof props.animationData === "string"
-      ? JSON.parse(props.animationData)
+    typeof props.src === "string"
+      ? props.src
       : props.animationData;
   return (
     <Lottie
       {...props}
-      animationData={animationData}
+      animationData={typeof animationData === 'string' ? undefined : animationData}
+      path={typeof animationData === 'string' ? animationData : undefined}
       className="w-full h-full"
       loop
       autoplay
