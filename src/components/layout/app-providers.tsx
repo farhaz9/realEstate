827bc5a882f3e448478e40ac60ca27eecda747dd
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-11-25 11:16:07 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/layout/app-providers.tsx
+++ src/components/layout/app-providers.tsx
@@ -4,7 +4,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/layout/footer";
 import MobileNav from "./mobile-nav";
-import { FirebaseClientProvider } from "@/firebase/client-provider";
 
 export default function AppProviders({
   children,
@@ -12,13 +11,13 @@ export default function AppProviders({
   children: React.ReactNode;
 }) {
   return (
-    <FirebaseClientProvider>
+    <>
       <main className="min-h-[calc(100vh-theme(spacing.16))] pb-16 md:pb-0">
         {children}
       </main>
       <MobileNav />
       <Footer />
       <Toaster />
-    </FirebaseClientProvider>
+    </>
   );
 }
