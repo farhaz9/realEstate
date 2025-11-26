c07a066ba7b3c1dbffa201dbb120c7c51d468034
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-11-26 17:55:44 +0000

    FirebaseError: Firebase Storage: No default bucket found. Did you set th

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-6989464421-8f3d7",
   "appId": "1:810301537756:web:f51cf424944bb3a7686319",
+  "storageBucket": "studio-6989464421-8f3d7.appspot.com",
   "apiKey": "AIzaSyBC7pH-nwZ_dbswYHzXLDJCt6W7i6d3qiM",
   "authDomain": "studio-6989464421-8f3d7.firebaseapp.com",
   "measurementId": "",
