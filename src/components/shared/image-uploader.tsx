0610ae77baed58ed1018b2853d1b13ebd8a25de8
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-11-27 05:45:22 +0000

    Try fixing this error: `Console Error: React does not recognize the `ima

--- src/components/shared/image-uploader.tsx
+++ src/components/shared/image-uploader.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { useImageKit } from '@/imagekit/provider';
 import { IKUpload } from 'imagekitio-react';
 import { Button } from '@/components/ui/button';
