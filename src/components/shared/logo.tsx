31e104e133aa9f6172cad5f0b8f7a58cdefdf182
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-14 11:53:11 +0000

    keep text small for logo so it fit in top navbar

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -6,12 +6,11 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 w-fit">
        <div className="text-center">
-        <div className="text-3xl font-semibold tracking-wide text-black">
-        FALCON AXE
+        <div className="text-xl font-semibold tracking-normal text-black">
+          FALCON AXE
         </div>
-
-        <div className="mt-1 text-xs tracking-[0.4em] text-purple-700">
-        HOMES
+        <div className="mt-0.5 text-[0.6rem] tracking-[0.3em] text-purple-700">
+          H O M E S
         </div>
     </div>
     </Link>
