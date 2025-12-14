00351c1e414e6f3fd0b5983d63716f19695b2988
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-14 11:55:06 +0000

    i want small logo text for mobile and desktop

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,4 +1,3 @@
-
 import Link from 'next/link';
 import { Building2 } from 'lucide-react';
 
@@ -6,10 +5,10 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 w-fit">
        <div className="text-center">
-        <div className="text-2xl font-semibold tracking-wide text-black">
+        <div className="text-xl font-semibold tracking-wide text-black">
           FALCON AXE
         </div>
-        <div className="mt-1 text-xs tracking-[0.4em] text-purple-700">
+        <div className="mt-1 text-[0.6rem] tracking-[0.3em] text-purple-700">
           H O M E S
         </div>
     </div>
