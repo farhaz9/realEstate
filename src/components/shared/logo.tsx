5f8cc90be1e9d18d2d1c5d008ef71a7d2a71341d
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-14 11:51:06 +0000

    also change name in top navbar form estately to falcon axe homes

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,13 +1,19 @@
+
 import Link from 'next/link';
 import { Building2 } from 'lucide-react';
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 w-fit">
-      <div className="flex items-center justify-center size-10 rounded-full bg-primary/10 text-primary">
-          <Building2 className="h-6 w-6"/>
-      </div>
-      <h1 className="text-xl font-bold tracking-tight">Estately</h1>
+       <div className="text-center">
+        <div className="text-3xl font-semibold tracking-wide text-black">
+        FALCON AXE
+        </div>
+
+        <div className="mt-1 text-xs tracking-[0.4em] text-purple-700">
+        HOMES
+        </div>
+    </div>
     </Link>
   );
 }
