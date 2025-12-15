8a9278c6e609460859902881d9962d4ed53ac012
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-15 06:10:50 +0000

    you can zoom in the image so image fit perfectly

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -258,8 +258,8 @@ export default function LoginPage() {
           src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Modern minimalist living room"
           layout="fill"
-          objectFit="contain"
-          className="absolute inset-0 py-12"
+          objectFit="cover"
+          className="absolute inset-0"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20"></div>
 
