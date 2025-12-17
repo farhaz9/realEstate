ed1bc40d7753d140a87d8abaf9af1631503005c9
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-17 18:36:34 +0000

    in background section  use another color because card and section is whi

--- src/components/shared/popular-properties.tsx
+++ src/components/shared/popular-properties.tsx
@@ -29,7 +29,7 @@ export function PopularProperties() {
   }
   
   return (
-    <section id="popular-listings" className="py-16 md:py-24 bg-background">
+    <section id="popular-listings" className="py-16 md:py-24 bg-primary/5">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center mb-8">
           <h2 className="text-3xl md:text-4xl font-bold">{title}</h2>
