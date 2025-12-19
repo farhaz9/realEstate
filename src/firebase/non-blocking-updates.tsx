b05e8cb1f48ceff95f2c99c81da41d2f93d92115
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-19 17:24:49 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Missing

--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -1,3 +1,4 @@
+
 'use client';
     
 import {
@@ -23,7 +24,7 @@ export function setDocumentNonBlocking(docRef: DocumentReference, data: any, opt
       'permission-error',
       new FirestorePermissionError({
         path: docRef.path,
-        operation: 'write', // or 'create'/'update' based on options
+        operation: options.merge ? 'update' : 'create',
         requestResourceData: data,
       })
     )
