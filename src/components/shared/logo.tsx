d7e411b2a9f8181c7041c2965709cc8c56ae6f2d
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-20 12:17:10 +0000

    falcon estates preloader text small in mobile and desktop view

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -5,10 +5,10 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 w-fit">
        <div className="text-center">
-        <div className="text-base font-semibold tracking-wide text-black">
+        <div className="text-3xl font-semibold tracking-wide text-black">
           FALCON
         </div>
-        <div className="text-[0.45rem] tracking-[0.2em] text-purple-700">
+        <div className="text-xs tracking-[0.4em] text-purple-700">
           ESTATES
         </div>
     </div>
