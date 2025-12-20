0b49aa64c98b5b940eae555ffa09bfde8ffee843
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-20 16:02:58 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBC7pH-nwZ_dbswYHzXLDJCt6W7i6d3

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-6989464421-8f3d7",
-  "appId": "1:810301537756:web:f51cf424944bb3a7686319",
-  "storageBucket": "studio-6989464421-8f3d7.appspot.com",
-  "apiKey": "AIzaSyBC7pH-nwZ_dbswYHzXLDJCt6W7i6d3qiM",
-  "authDomain": "studio-6989464421-8f3d7.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "810301537756"
+  apiKey: "AIzaSyBC7pH-nwZ_dbswYHzXLDJCt6W7i6d3qiM",
+  authDomain: "estates.falconaxe.com",
+  projectId: "studio-6989464421-8f3d7",
+  storageBucket: "studio-6989464421-8f3d7.firebasestorage.app",
+  messagingSenderId: "810301537756",
+  appId: "1:810301537756:web:f51cf424944bb3a7686319"
 };
