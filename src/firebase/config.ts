3ef75b870103476a5fe7da283ff7b67d25360849
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-12-20 16:12:57 +0000

    Choose an account to continue to studio-6989464421-8f3d7.firebaseapp.com

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBC7pH-nwZ_dbswYHzXLDJCt6W7i6d3qiM",
-  authDomain: "studio-6989464421-8f3d7.firebaseapp.com",
+  authDomain: "estates.falconaxe.com",
   projectId: "studio-6989464421-8f3d7",
   storageBucket: "studio-6989464421-8f3d7.firebasestorage.app",
   messagingSenderId: "810301537756",
